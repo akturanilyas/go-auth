f839ba69ce3b76c7742cff7895b7410bd67400e4
Ilyas AKTURAN <akturanilyas@gmail.com>
2024-09-14 22:58:22 +0300

    Refactor architecture

--- pkg/auth/auth_controller.go
+++ pkg/auth/auth_controller.go
@@ -1,11 +1,8 @@
 package auth
 
 import (
-	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 )
-
-var Validate = validator.New()
 
 func Login(c *fiber.Ctx) error {
 	loginData := new(LoginValidation)
@@ -44,38 +41,11 @@ func Logout(c *fiber.Ctx) error {
 }
 
 func RefreshToken(c *fiber.Ctx) error {
-	refreshToken := c.Get("Authorization")
-	if refreshToken == "" {
-		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"error": "Missing refresh token",
-		})
-	}
-
-	newToken, err := RefreshTokenService(refreshToken)
-	if err != nil {
-		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"error": "Invalid refresh token",
-		})
-	}
-
-	return c.JSON(fiber.Map{
-		"token": newToken,
-	})
+	// Implement token refresh logic here
+	return c.SendStatus(fiber.StatusOK)
 }
 
 func ForgotPassword(c *fiber.Ctx) error {
-	email := c.FormValue("email")
-	if email == "" {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"error": "Email is required",
-		})
-	}
-
-	if err := ForgotPasswordService(email); err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error": "Could not process forgot password request",
-		})
-	}
-
+	// Implement password reset logic here
 	return c.SendStatus(fiber.StatusOK)
 }
--- pkg/auth/auth_service.go
+++ pkg/auth/auth_service.go
@@ -2,13 +2,9 @@ package auth
 
 import (
 	"auth-go/pkg/database"
-	"errors"
-	"time"
-
 	"auth-go/pkg/user"
 	"auth-go/utils"
 
-	"github.com/dgrijalva/jwt-go"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -66,51 +62,10 @@ func LoginService(loginData *LoginValidation) (string, error) {
 	return token, nil
 }
 
-func RefreshTokenService(refreshToken string) (string, error) {
-	claims := &utils.JWTClaim{}
-	token, err := jwt.ParseWithClaims(refreshToken, claims, func(token *jwt.Token) (interface{}, error) {
-		a, _ := utils.GetJWTSecret()
-		return a, nil
-	})
+func RefreshTokenService(refreshToken string) {
 
-	if err != nil {
-		return "", err
-	}
-
-	if !token.Valid {
-		return "", errors.New("invalid refresh token")
-	}
-
-	if time.Unix(claims.ExpiresAt, 0).Before(time.Now()) {
-		return "", errors.New("refresh token is expired")
-	}
-
-	var user user.User
-	if err := database.DB.Where("email = ?", claims.Email).First(&user).Error; err != nil {
-		return "", err
-	}
-
-	// Generate a new access token
-	newAccessToken, err := utils.GenerateJWT(user.Email)
-	if err != nil {
-		return "", err
-	}
-
-	return newAccessToken, nil
 }
 
-func ForgotPasswordService(email string) error {
-	resetToken := utils.GenerateResetToken()
+func ForgotPasswordService(email string) {
 
-	var user user.User
-	if err := database.DB.Where("email = ?", email).First(&user).Error; err != nil {
-		return err
-	}
-
-	user.ResetToken = resetToken
-	if err := database.DB.Save(&user).Error; err != nil {
-		return err
-	}
-	// TODO: Send reset password email
-	return nil
 }
